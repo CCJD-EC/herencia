4e0b3236dfd66870e615e6133d3717b8432c6940
Christian <jchristiandias@gmail.com>
2020-12-10 11:25:10 -0500

    Ejercicio 3 parte 1 completado

--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -1,18 +1,68 @@
 package com.clearminds.impl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.clearminds.excepciones.InstanceException;
 import com.clearminds.interfaces.ServicioPersona;
 import com.clearminds.model.Persona;
 
 public class PersonaManager {
 
 		private ServicioPersona serv;
+		public static String directorio = "./config.properties";
 		
-		public PersonaManager() {
+		public PersonaManager() throws InstanceException {
+			Class<?> clase = null;
+			try {
+				clase = Class.forName("com.clearminds.impl.ServicioPersonaBDD");
+				this.serv = (ServicioPersona)clase.newInstance();
+			} catch (Exception e) {
+				throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+			}
 			
-			ServicioPersonaBDD serv = new ServicioPersonaBDD();
 		}
 		
 		public void insertarPersona(Persona persona) {
 			serv.insertar(persona);
+		}
+	
+
+		public static String leerPropiedad(String nombre) {
+
+			File f = new File(directorio);
+
+			BufferedReader br = null;
+			FileReader fr = null;
+			try {
+				fr = new FileReader(f);
+				br = new BufferedReader(fr);
+				String linea = "";
+				String valor = "";
+
+				while ((linea = br.readLine()) != null) {
+					
+					String[] propiedad = linea.split(":", 2);
+					if (propiedad[0].equals(nombre))
+						return propiedad[1];
+				}
+
+			} catch (FileNotFoundException e) {
+				System.out.println("No se encontro el archivo " + directorio);
+				e.printStackTrace();
+			} catch (Exception e) {
+				System.out.println("Problema al leer el archivo");
+				e.printStackTrace();
+			} finally {
+				try {
+					fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return null;
 		}
 }
--- src/main/java/com/clearminds/impl/TestManager.java
+++ src/main/java/com/clearminds/impl/TestManager.java
@@ -1,24 +1,18 @@
 package com.clearminds.impl;
 
+import com.clearminds.excepciones.InstanceException;
 import com.clearminds.model.Persona;
 
 public class TestManager {
 
 	public static void main(String[] args) {
-		PersonaManager persManager = new PersonaManager();
+		PersonaManager persManager;
+		try {
+			persManager = new PersonaManager();
+			persManager.insertarPersona(new Persona("marcos", "milano", 10));
+		} catch (InstanceException e) {
+			e.printStackTrace();
+		}
 		
-		persManager.insertarPersona(new Persona("marcos", "milano", 10));
-		
-		
-		/*Al intentar ejecutar este codigo produce un nullPointerException
-		 * debido a que en el constructor se instancia el objeto de tipo
-		 * ServicioPersonaBDD pero la funcion toma el atributo de la clase para realizar la 
-		 * operacion y como no tiene valor queda en null.
-		 * 
-		 * ServicioPersonaBDD este objeto al ser una variable local su ciclo 
-		 * solo es dentro del metodo en este caso el constructor y muere cuando
-		 * se termina de usar el constructor y sus valores se pierden
-		 * 
-		 * */
 	}
 }
